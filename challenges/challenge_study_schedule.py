a321341e82f03f62b52e788a1db598fa6003c9d4
ThiagoZardo <thiago.artevisual@gmail.com>
2022-11-18 19:08:49 -0300

    Req 2 ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,7 @@ def study_schedule(permanence_period, target_time):
         return None
     
     connected_students = 0
-    for login, logout in permanence_period:
+    for login, logout in permanence_period: # Aqui atribuo login e logout para os indices 0 e 1.
         if not isinstance(login, int) or not isinstance(logout, int):
             return None
         if login <= target_time <= logout:
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 def test_encrypt_message():
-    pass
+    message = 'development'
+
+    for key in range(2, len(message)):
+        encrypt = encrypt_message(message, key)
+        if key % 2 != 0:
+            part_one = message[:key]
+            part_two = message[key:]
+            assert encrypt == part_one[::-1] + '_' + part_two[::-1]
+        else:
+            part_one = message[:key]
+            part_two = message[key:]
+            assert encrypt == part_two[::-1] + '_' + part_one[::-1]
+
+    encrypt = encrypt_message(message, len(message)+1)
+    assert encrypt == message[::-1]
+
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message(message, '1')
+
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(1, 1)
