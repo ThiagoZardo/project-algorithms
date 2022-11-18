21766dd09a1521a3d8bc3d9a6fb3a15a3db34014
ThiagoZardo <thiago.artevisual@gmail.com>
2022-11-18 16:30:08 -0300

    Requisito 01 ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    
+    connected_students = 0
+    for login, logout in permanence_period:
+        if not isinstance(login, int) or not isinstance(logout, int):
+            return None
+        if login <= target_time <= logout:
+            connected_students += 1
+    return connected_students
