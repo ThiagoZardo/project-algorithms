ac487254fedfdaa86bc70716291ea866e75e0f97
ThiagoZardo <thiago.artevisual@gmail.com>
2022-11-22 00:28:15 -0300

    req 04 ok

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,45 @@
+def merge_sort(string, start=0, end=None):
+    if end is None:
+        end = len(string)
+
+    if (end - start) > 1: # se não reduzi o suficiente, continua
+        mid = (start + end) // 2 # encontrando o meio
+        merge_sort(string, start, mid) # dividindo as listas
+        merge_sort(string, mid, end)
+        merge(string, start, mid, end) # unindo as listas
+
+
+def merge(string, start, mid, end):
+    left = string[start:mid]
+    right = string[mid:end]
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            string[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            string[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            string[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            string[general_index] = right[right_index]
+            right_index = right_index + 1
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first = list(first_string.lower())
+    secound = list(second_string.lower())
+
+    merge_sort(first, 0, len(first_string))
+    merge_sort(secound, 0, len(second_string))
+
+    first_string_sorted = ''.join(first)
+    secound_string_sorted = ''.join(secound)
+
+    if (first_string_sorted == '' or secound_string_sorted == ''):
+        return (first_string_sorted, secound_string_sorted, False)
+
+    return (first_string_sorted, secound_string_sorted, first_string_sorted == secound_string_sorted)
